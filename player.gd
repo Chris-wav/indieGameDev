449b21474a59feca70d60de97018a55edcaa4141
christos kalatzis <christos@Mac.home>
2026-06-11 23:44:45 +0300

    basic player movement left-right and jump

--- player.gd
+++ player.gd
@@ -1,1 +1,22 @@
-extends CharacterBody2D;
+extends CharacterBody2D
+
+@export var speed = 200
+@export var jump_velocity = -350
+@export var gravity = 900
+
+func _physics_process(delta):
+	# Gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	var direction = Input.get_axis("move_left", "move_right")
+
+	if direction != 0:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	move_and_slide()
